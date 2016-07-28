cb1becf3107ef6ce1bd817a5769a76bb9b550ba9
sjmarsh <sjmarsh31@gmail.com>
2016-07-28 14:57:40 +1000

    Create basic angular client
    - add member functionality working
    - no styles or templates yet

---
+++ AngularOneSpike/app/app.js
@@ -0,0 +1,20 @@
+﻿var app = angular.module('memberApp', []);
+
+app.controller('memberController', ['$scope', 'memberService', function ($scope, memberService) {
+
+  $scope.status = 'OK';
+  $scope.member = {};
+  $scope.genders = ['Male', 'Female']; // todo: grab these from a service
+
+  $scope.addMember = function () {
+    memberService.addMember($scope.member)
+    .success(function (id) {
+      $scope.status = 'Member added with id: ' + id;
+      $scope.member = {};
+    })
+    .error(function (error) {
+      $scope.status = 'Error occured adding member. Error Message: ' + error.message;
+    })
+  };
+
+}]);
---
+++ AngularOneSpike/app/memberService.js
@@ -0,0 +1,17 @@
+﻿app.service('memberService', ['$http', function ($http) {
+
+  var baseUrl = 'http://localhost:4963/api/member';
+
+  this.getMember = function (id) {
+    return $http.get(baseUrl + '/' + id);
+  };
+
+  this.getMemberList = function () {
+    return $http.get(baseUrl);
+  }
+
+  this.addMember = function (member) {
+    return $http.post(baseUrl, member);
+  }
+
+}]);
